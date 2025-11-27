326752b56df2f08639bcc6a2aec8da78e56c41ff
getanaka <241708680+getanaka@users.noreply.github.com>
2025-11-27 14:03:32 +0900

    Implement fetch_feeds tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,19 @@
 import pytest
 from typing import Generator
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import Session, SQLModel, create_engine, select
 from sqlmodel.pool import StaticPool
+import feedparser
+from datetime import datetime
 
-from feedreader3.main import FeedSource, app, get_session
+from feedreader3.main import (
+    FeedSource,
+    FeedEntry,
+    FeedEntryCreate,
+    app,
+    get_session,
+    fetch_feeds,
+)
 
 
 @pytest.fixture(name="session")
@@ -115,3 +124,146 @@ def test_delete_feed_source(session: Session, client: TestClient) -> None:
 
     assert response.status_code == 204
     assert db_feed_source0 is None
+
+
+def test_fetch_feeds_insert(session: Session) -> None:
+    feed_source = FeedSource(name="test_feed", feed_url="tests/atom10.xml")
+    session.add(feed_source)
+    session.commit()
+    session.refresh(feed_source)
+
+    parsed_feed = feedparser.parse(feed_source.feed_url)
+    parsed_entry = parsed_feed.entries[0]
+
+    fetch_feeds(session)
+
+    results = session.exec(
+        select(FeedEntry).where(FeedEntry.feed_source_id == feed_source.id)
+    ).all()
+    assert len(results) == 1
+    assert results[0].id is not None
+    assert results[0].updated_at is not None
+    assert results[0].first_seen_at is not None
+    assert results[0].feed_source_id == feed_source.id
+    assert results[0].entry_id == parsed_entry.id
+    assert results[0].entry_title == parsed_entry.title
+    assert results[0].entry_link == parsed_entry.link
+    # SQLite cannot have timezone so we need to compare datetime without tzinfo.
+    assert results[0].entry_updated_at == datetime(*parsed_entry.updated_parsed[:6])
+
+
+def test_fetch_feeds_update(session: Session) -> None:
+    feed_source = FeedSource(name="test_feed", feed_url="tests/atom10.xml")
+    session.add(feed_source)
+    session.commit()
+    session.refresh(feed_source)
+
+    feed_entry_create = FeedEntryCreate(
+        first_seen_at=datetime.now(),
+        feed_source_id=feed_source.id,
+        entry_id="tag:feedparser.org,2005-11-09:/docs/examples/atom10.xml:3",
+        entry_title="",
+        entry_link="",
+        entry_updated_at=datetime.min,
+    )
+    db_feed_entry = FeedEntry.model_validate(feed_entry_create)
+    session.add(db_feed_entry)
+    session.commit()
+
+    parsed_feed = feedparser.parse(feed_source.feed_url)
+    parsed_entry = parsed_feed.entries[0]
+
+    fetch_feeds(session)
+
+    results = session.exec(
+        select(FeedEntry).where(FeedEntry.feed_source_id == feed_source.id)
+    ).all()
+    assert len(results) == 1
+    assert results[0].id is not None
+    assert results[0].updated_at is not None
+    assert results[0].first_seen_at is not None
+    assert results[0].feed_source_id == feed_source.id
+    assert results[0].entry_id == feed_entry_create.entry_id
+    assert results[0].entry_id == parsed_entry.id
+    assert results[0].entry_title == parsed_entry.title
+    assert results[0].entry_link == parsed_entry.link
+    # SQLite cannot have timezone so we need to compare datetime without tzinfo.
+    assert results[0].entry_updated_at == datetime(*parsed_entry.updated_parsed[:6])
+
+
+def test_fetch_feeds_add_atom_without_published(session: Session) -> None:
+    feed_source = FeedSource(name="test_feed", feed_url="tests/atom11.xml")
+    session.add(feed_source)
+    session.commit()
+    session.refresh(feed_source)
+
+    parsed_feed = feedparser.parse(feed_source.feed_url)
+    parsed_entry = parsed_feed.entries[0]
+
+    fetch_feeds(session)
+
+    results = session.exec(
+        select(FeedEntry).where(FeedEntry.feed_source_id == feed_source.id)
+    ).all()
+    assert len(results) == 1
+    assert results[0].id is not None
+    assert results[0].updated_at is not None
+    assert results[0].first_seen_at is not None
+    assert results[0].feed_source_id == feed_source.id
+    assert results[0].entry_id == parsed_entry.id
+    assert results[0].entry_title == parsed_entry.title
+    assert results[0].entry_link == parsed_entry.link
+    # SQLite cannot have timezone so we need to compare datetime without tzinfo.
+    assert results[0].entry_updated_at == datetime(*parsed_entry.updated_parsed[:6])
+
+
+def test_fetch_feeds_add_atom_without_published_and_updated(session: Session) -> None:
+    feed_source = FeedSource(name="test_feed", feed_url="tests/atom12.xml")
+    session.add(feed_source)
+    session.commit()
+    session.refresh(feed_source)
+
+    parsed_feed = feedparser.parse(feed_source.feed_url)
+    parsed_entry = parsed_feed.entries[0]
+
+    fetch_feeds(session)
+
+    results = session.exec(
+        select(FeedEntry).where(FeedEntry.feed_source_id == feed_source.id)
+    ).all()
+    assert len(results) == 1
+    assert results[0].id is not None
+    assert results[0].updated_at is not None
+    assert results[0].first_seen_at is not None
+    assert results[0].feed_source_id == feed_source.id
+    assert results[0].entry_id == parsed_entry.id
+    assert results[0].entry_title == parsed_entry.title
+    assert results[0].entry_link == parsed_entry.link
+    # SQLite cannot have timezone so we need to compare datetime without tzinfo.
+    assert results[0].entry_updated_at is None
+
+
+def test_fetch_feeds_add_atom_has_duplicated_id_entries(session: Session) -> None:
+    feed_source = FeedSource(name="test_feed", feed_url="tests/atom13.xml")
+    session.add(feed_source)
+    session.commit()
+    session.refresh(feed_source)
+
+    parsed_feed = feedparser.parse(feed_source.feed_url)
+    parsed_entry = parsed_feed.entries[1]
+
+    fetch_feeds(session)
+
+    results = session.exec(
+        select(FeedEntry).where(FeedEntry.feed_source_id == feed_source.id)
+    ).all()
+    assert len(results) == 1
+    assert results[0].id is not None
+    assert results[0].updated_at is not None
+    assert results[0].first_seen_at is not None
+    assert results[0].feed_source_id == feed_source.id
+    assert results[0].entry_id == parsed_entry.id
+    assert results[0].entry_title == parsed_entry.title
+    assert results[0].entry_link == parsed_entry.link
+    # SQLite cannot have timezone so we need to compare datetime without tzinfo.
+    assert results[0].entry_updated_at == datetime(*parsed_entry.updated_parsed[:6])
