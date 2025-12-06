c6cbc737aa24520bbaf98f924db0d84c4a361f0a
getanaka <241708680+getanaka@users.noreply.github.com>
2025-12-06 14:18:39 +0900

    Add conftest module and move fixtures to it

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,42 +1,14 @@
-import pytest
-from typing import Generator
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine, select
-from sqlmodel.pool import StaticPool
+from sqlmodel import Session, select
 import feedparser
 from datetime import datetime, timedelta
 
 from feedreader3.main import (
     FeedEntry,
     FeedEntryCreate,
-    app,
     fetch_feeds,
 )
-from feedreader3.dependencies import get_session
 from feedreader3.models.feed_source import FeedSource
-
-
-@pytest.fixture(name="session")
-def session_fixture() -> Generator[Session, None, None]:
-    engine = create_engine(
-        "sqlite://",
-        connect_args={"check_same_thread": False},
-        poolclass=StaticPool,
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session
-
-
-@pytest.fixture(name="client")
-def client_fixture(session: Session) -> Generator[TestClient, None, None]:
-    def get_session_override() -> Session:
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
 
 
 def test_create_feed_source(client: TestClient) -> None:
