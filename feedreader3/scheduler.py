509ef5efc52b3f6fa36e41835b4f76d077bced27
getanaka <241708680+getanaka@users.noreply.github.com>
2025-12-16 22:01:24 +0900

    Add misfire_grace_time and coalesce configurations to background_scheduler

--- feedreader3/scheduler.py
+++ feedreader3/scheduler.py
@@ -5,6 +5,8 @@ from .jobs.fetch_feeds_job import fetch_feeds_job
 background_scheduler = BackgroundScheduler()
 
 
-@background_scheduler.scheduled_job(CronTrigger.from_crontab("*/10 * * * *"))  # type: ignore[misc]
+@background_scheduler.scheduled_job(
+    CronTrigger.from_crontab("*/10 * * * *"), misfire_grace_time=30, coalesce=True
+)  # type: ignore[misc]
 def scheduled_job() -> None:
     fetch_feeds_job()
