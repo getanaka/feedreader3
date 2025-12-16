e3f10abbad7ed82cfea6d056632122c93ac9f18f
getanaka <241708680+getanaka@users.noreply.github.com>
2025-12-16 12:43:47 +0900

    Extract scheduler module

--- feedreader3/main.py
+++ feedreader3/main.py
@@ -1,19 +1,7 @@
 from fastapi import FastAPI
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.cron import CronTrigger
 from .database import create_db_and_tables
 from .routers import feed_sources, feed_entries
-from .jobs.fetch_feeds_job import fetch_feeds_job
-
-
-# Worker
-scheduler = BackgroundScheduler()
-
-
-@scheduler.scheduled_job(CronTrigger.from_crontab("*/10 * * * *"))  # type: ignore[misc]
-def scheduled_job() -> None:
-    fetch_feeds_job()
-
+from .scheduler import background_scheduler
 
 # Web
 app = FastAPI()
@@ -23,12 +11,12 @@ app = FastAPI()
 def on_startup() -> None:
     # TODO: Run migration script
     create_db_and_tables()
-    scheduler.start()
+    background_scheduler.start()
 
 
 @app.on_event("shutdown")
 def on_shutdown() -> None:
-    scheduler.shutdown()
+    background_scheduler.shutdown()
 
 
 app.include_router(feed_sources.router)
