d5f78578db81622c79425bf01e0f8de7c6836a61
getanaka <241708680+getanaka@users.noreply.github.com>
2026-01-04 17:38:50 +0900

    Add postgres configurations to settings module

--- feedreader3/settings.py
+++ feedreader3/settings.py
@@ -11,6 +11,12 @@ class Settings:
 
     database_file_name: str
 
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str
+    postgres_port: int
+
 
 _settings: Settings = Settings()
 
@@ -33,6 +39,16 @@ def initialize_settings(
 
     _settings.database_file_name = os.getenv("DATABASE_FILE_NAME", "database.db")
     logger.info(f"settings.database_file_name={_settings.database_file_name}")
+
+    _settings.postgres_user = os.getenv("POSTGRES_USER", "postgres")
+    logger.info(f"settings.postgres_user={_settings.postgres_user}")
+    _settings.postgres_password = os.getenv("POSTGRES_PASSWORD", "password")
+    _settings.postgres_db = os.getenv("POSTGRES_DB", "postgres")
+    logger.info(f"settings.postgres_db={_settings.postgres_db}")
+    _settings.postgres_host = os.getenv("POSTGRES_HOST", "db")
+    logger.info(f"settings.postgres_host={_settings.postgres_host}")
+    _settings.postgres_port = int(os.getenv("POSTGRES_PORT", 5432))
+    logger.info(f"settings.postgres_port={_settings.postgres_port}")
 
 
 def get_settings() -> Settings:
